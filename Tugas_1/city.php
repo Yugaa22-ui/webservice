955399dcaebbb370448045fd365ffb398b317435
LaluYoga <bangjoox77@gmail.com>
2025-10-16 08:07:59 +0800

    perbaiki cas PUT city.php

--- Tugas_1/city.php
+++ Tugas_1/city.php
@@ -34,10 +34,18 @@ switch ($method) {
             exit();
         }
 
-        $data = json_decode(file_get_contents("php://input"), true);
+        $raw = file_get_contents("php://input");
+        $data = json_decode($raw, true);
+
+        // Jika JSON gagal terbaca, coba baca data urlencoded
+        if ($data === null) {
+            parse_str($raw, $data);
+        }
+
         updateCity($conn, $_GET['id'], $data);
         break;
 
+        
     case "DELETE":
         if (!isset($_GET['id'])) {
             echo json_encode([
